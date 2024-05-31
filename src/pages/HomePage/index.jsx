0581506d5438c550e94cda2eb2e4c3ea0fddf9a2
ThienTran98 <120354679+ThienTran98@users.noreply.github.com>
2024-05-31 21:08:49 +0700

    update

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function HomePage() {
-  return <div>index</div>;
+  return (
+    <div>
+      <div>
+        <label htmlFor="files" className="btn">
+          Select Image
+        </label>
+        <input id="files" style={{ visibility: "hidden" }} type="file" />
+      </div>
+    </div>
+  );
 }
